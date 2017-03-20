80f78ecf18e5f96cae9c2ec8902e9310319003ad
Anton Kiselev <antonxkiselev@gmail.com>
2017-03-20 19:31:44 +0300

    enum

--- lottie-ios/Classes/Private/LOTAnimationView_Internal.h
+++ lottie-ios/Classes/Private/LOTAnimationView_Internal.h
@@ -11,7 +11,7 @@
 typedef enum : NSUInteger {
   LOTConstraintTypeAlignToBounds,
   LOTConstraintTypeAlignToLayer,
-  LOTConstraintTypeMatchReferenceView,
+  LOTConstraintTypeMatchReferenceLayer,
   LOTConstraintTypeNone
 } LOTConstraintType;
 
